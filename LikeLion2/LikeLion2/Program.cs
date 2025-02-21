e879949ff0da269be93ce0d63818f88fe91e004b
manaqua138 <manaqua1231@gmail.com>
2025-02-21 10:23:01 +0900

    study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            double pi = 3.14;
+            char letter = 'A';
+            string name = "ALice";
+            int age = 25;
+
+            Console.WriteLine(age);
+            Console.WriteLine(pi);
+            Console.WriteLine(letter);
+            Console.WriteLine(name);
+
+
+
+        }
+    }
+}
