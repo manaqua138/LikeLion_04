bf95319f6a7231621ad901568cbf562138cf2b55
manaqua138 <manaqua1231@gmail.com>
2025-02-21 17:37:53 +0900

    study5

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 로딩바 시작화면
+            //게임스토리 1
+            //콘솔 텍스트 노벨 만들기
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("엔터를 치면 스토리가 시작됩니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("아침에 눈이 내리고 있었다.");
+            Console.ReadLine();
+            Console.WriteLine("창문을 열자 차가운 공기가 밀려 들어왔다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("창밖으로 고개를 내밀어보니, 밤새 내린 눈이 도로위에 얇게 쌓여 있었고,");
+            Console.ReadLine();
+            Console.WriteLine("사람들은 주머니에 손을 찔러 넣은채 발걸음을 서둘렀다.");
+            Console.ReadLine();
+            Console.WriteLine("바람이 불 때마다 눈발이 흐트러지며 흩날렸다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("거리를 따라 천천히 시선을 옮기자 길모퉁이 담장 위로 고양이 한 마리가 보였다.");
+            Console.ReadLine();
+            Console.WriteLine("마치 정해진 경로라도 있는 듯, 고양이는 일정한 보폭으로 담장 위를 걸었다.");
+            Console.ReadLine();
+            Console.WriteLine("나는 그 모습을 한동안 바라보다가, 천천히 시선을 거두었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그리고 커피를 내리기 위해 전기 주전자를 켰다.");
+            Console.ReadLine();
+            Console.WriteLine("물은 금방 끊었고, 김은 천천히 피어올랐다.");
+            Console.ReadLine();
+            Console.WriteLine("커피를 내리는 일은 이미 익숙한 동작이었고, 나의 삶의 일부처럼 자연스러웠다. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("커피 잔을 들고 창가로 다가갔다.");
+            Console.ReadLine();
+            Console.WriteLine("눈은 끊임없이 내리고 있었고, 거리에는 조금 전보다 더 많은 눈이 쌓여 있었다.");
+            Console.ReadLine();
+            Console.WriteLine("창 밖을 바라보며 커피를 한 모금 마셨다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("눈이 내리면 세상은 조금 조용해지는 것 같다.");
+            Console.ReadLine();
+            Console.WriteLine("길바닥의 얼룩도, 떨어진 종이컵도, 갈라진 아스팔트도, 모든 것이 하얀색 아래 감춰진다.");
+            Console.ReadLine();
+            Console.WriteLine("사람들의 얼굴도 마찬가지였다.");
+            Console.ReadLine();
+            Console.WriteLine("흐릿한 하늘 아래, 모두 비슷한 색의 옷을 입고, 비슷한 표정을 한 채 걸음을 재촉했다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("눈이 내릴 때면 나는 가끔 생각한다.");
+            Console.ReadLine();
+            Console.WriteLine("이 도시가 본래부터 이렇게 깨끗한 곳이었다면 어땠을까");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
