7bdaeb986f4fb3674c928f63a2a3b7cabc05750f
manaqua138 <manaqua1231@gmail.com>
2025-03-05 17:59:31 +0900

    25_03_05_study

---
+++ 25_03_05/25_03_05/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_03_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = { "Charlie", "Alice", "Bob" };
+            //var sortedNames = names.OrderBy(n => n);
+
+            //foreach(var name in sortedNames)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //var firstName = names.First(n => n.StartsWith("A"));
+
+            //Console.WriteLine($"First name starting with A: {firstName}");
+
+            //int[] nums = { 5, 3, 8, 1 };
+
+            //var sortedMeshod = nums.OrderBy(n => n);
+
+            //var sortedQuery = from n in nums
+            //                  orderby n
+            //                  select n;
+
+            //Console.WriteLine("Method syntax : ");
+            //foreach (var n in sortedMeshod) Console.WriteLine(n);
+
+            //Console.WriteLine("Query syntax : ");
+            //foreach (var n in sortedQuery) Console.WriteLine(n);
+
+            //select 개념
+            //Linq 쿼리 연산자 중 하나
+            //각 요소를 변환하여 새로운 컬렉션을 생성
+
+            //string[] words = { "apple", "banana", "cherry" };
+
+            //var upperWords = words.Select(w => w.ToUpper());
+
+            //foreach (var word in upperWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+            //int[] data = { 1, 2, 3, 4, 5 };
+            //int sum = 0;
+
+            //foreach (var d in data)
+            //    sum += d;
+
+            //Console.WriteLine($"Sum : {sum}");
+
+            //double avg = data.Average();
+            //Console.WriteLine($"Average : {avg}");
+
+            //int max = data.Max();
+
+            //Console.WriteLine($"Max :  {max}");
+
+            //int[] data = { 10, 20, 30, 40, 50, };
+            //int target = 22;
+            //int nearest = data[0];
+
+            //foreach(var num in data)
+            //{
+            //    if (Math.Abs(num - target) < Math.Abs(nearest - target))
+            //        nearest = num;
+            //}
+
+            //Console.WriteLine($"Nearest to {target} : {nearest}");
+
+            //int[] data = { 5, 2, 8, 1, 9 };
+            //Array.Sort(data);
+
+            //foreach (var d in data)
+            //    Console.WriteLine(d);
+
+            //특정 값 검색하기: Search 알고리즘
+
+            //int[] data = { 5, 2, 8, 1, 9 };
+            //int target = 8;
+            //int index = -1;
+
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    if (data[i] == target)
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(index >= 0 ? $"Found at index {index}" : "Not found");
+
+            //백준
+            //프로그래머스
+
+            //그룹화하기 : GROUP 알고리즘
+            //데이터를 특정 기준으로 그룹화하기
+
+            string[] fruits = { "apple", "banana", "blueberry", "cherry", "apricot" };
+
+            var groups = fruits.GroupBy(f => f[0]); //첫 글자로 그룹화
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"Key : {group.Key}");
+
+                foreach (var item in group)
+                {
+                    Console.WriteLine($" {item}");
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ 25_03_05_study2/25_03_05_study2/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study34
+{
+
+
+
+    //부모클래스 (기본유닛)
+    class Unit
+    {
+        public string Name { get; set; } = "Unknown";
+        public int Health { get; set; } = 0;
+        public int Mana { get; set; } = 0;
+        public int AttackDamage { get; set; } = 0;
+        public int SpellDamage { get; set; } = 0;
+
+        public List<string> Skills { get; set; } = new List<string>(10);        
+
+        public virtual void Attack()
+        {
+            Console.WriteLine($"{Name}이 기본 공격을 합니다. ");
+        }
+
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"{Name}이 이동합니다. ");
+        }
+    }
+
+    //SCV유닛 (건설과 수리 기능)
+    class Ezreal : Unit
+    {
+        public int PassiveStacks { get; set; } = 0;
+        public bool isWMarked { get; set; } = false;
+
+        public double W_damage { get; set; } = 0;
+        
+        public Ezreal()
+        {
+            Name = "Ezreal";
+            Health = 600;
+            Mana = 375;
+            AttackDamage = 60;
+            SpellDamage = 0;
+            Skills.Add("Rising_spell_force");
+            Skills.Add("Mystic_Shot");
+            Skills.Add("Essence_Flux");
+            Skills.Add("Arcane_Shift");
+            Skills.Add("Trueshot_Barrage");
+            W_damage = 80 + 1.0 * AttackDamage + 0.7 * SpellDamage;
+        }
+
+        public void PassiveSkill()
+        {
+            if (PassiveStacks < 5)
+                PassiveStacks++;
+        }
+
+        public void Q_Skill()
+        {
+            double Q_damage = 20 + 1.3 * AttackDamage + 0.15* SpellDamage;
+   
+            if (isWMarked == true)
+            {
+                Console.WriteLine($"상대방에게 {Q_damage + W_damage} 데미지를 입혔습니다!");
+                Mana += 60;
+                isWMarked = false;
+            }
+            else
+            {
+                Console.WriteLine($"상대방에게 {Q_damage} 데미지를 입혔습니다!");
+                Mana -= 28;
+            }
+
+            PassiveSkill();
+        }
+
+        public void W_Skill()
+        {
+
+            isWMarked = true;
+            Mana -= 50;
+            Console.WriteLine("정수의 흐름이 적중했습니다!");
+            PassiveSkill();
+
+        }
+
+        public void E_Skill()
+        {
+            double E_damage = 80 + 0.5 * AttackDamage + 0.75 * SpellDamage;
+
+            Console.WriteLine($"비전 이동으로 회피하였습니다!");
+
+            if (isWMarked == true)
+            {
+                Console.WriteLine($"상대방에게 {E_damage + W_damage} 데미지를 입혔습니다!");
+                Mana += 60;
+                isWMarked = false;
+            }
+            else
+            {
+                Console.WriteLine($"상대방에게 {E_damage} 데미지를 입혔습니다!");
+                Mana -= 70;
+            }
+            PassiveSkill();
+        }
+
+        public void R_Skill()
+        {
+            double R_damage = 350 + 1.0 * AttackDamage + 0.9 * SpellDamage;
+
+            Console.WriteLine($"정조준 일격을 발사하였습니다!");
+
+            if (isWMarked == true)
+            {
+                Console.WriteLine($"상대방에게 {R_damage + W_damage} 데미지를 입혔습니다!");
+                Mana += 60;
+                isWMarked = false;
+            }
+            else
+            {
+                Console.WriteLine($"상대방에게 {R_damage} 데미지를 입혔습니다!");
+                Mana -= 100;
+            }
+
+            PassiveSkill();
+        }
+
+
+    }
+
+
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+ 
+            Ezreal ezreal = new Ezreal();
+
+            Console.WriteLine($"챔피언: {ezreal.Name}");
+            Console.WriteLine($"체력: {ezreal.Health}, 마나: {ezreal.Mana}");
+            Console.WriteLine($"공격력: {ezreal.AttackDamage}, 주문력: {ezreal.SpellDamage}");
+            Console.WriteLine($"보유 스킬: {string.Join(", ", ezreal.Skills)}");
+            Console.WriteLine();
+
+            ezreal.Attack();
+            Console.WriteLine();
+
+            ezreal.W_Skill();
+            Console.WriteLine($"현재 마나: {ezreal.Mana}");
+            Console.WriteLine($"현재 패시브 스택: {ezreal.PassiveStacks}");
+            Console.WriteLine();
+
+            ezreal.Q_Skill();
+            Console.WriteLine($"현재 마나: {ezreal.Mana}");
+            Console.WriteLine($"현재 패시브 스택: {ezreal.PassiveStacks}");
+            Console.WriteLine();
+
+            ezreal.E_Skill();
+            Console.WriteLine($"현재 마나: {ezreal.Mana}");
+            Console.WriteLine($"현재 패시브 스택: {ezreal.PassiveStacks}");
+            Console.WriteLine();
+
+            ezreal.R_Skill();
+            Console.WriteLine($"현재 마나: {ezreal.Mana}");
+            Console.WriteLine($"현재 패시브 스택: {ezreal.PassiveStacks}");
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
